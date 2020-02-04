330b18fdc94f314f56952d8145fcb7f86227be75
phacic <phacic@msn.com>
2020-02-04 08:21:40 +0000

    initial commit

---
+++ cd10/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Cd10Config(AppConfig):
+    name = 'cd10'
---
+++ cd10/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+
+class Category(models.Model):
+    # short alphanumeric code for the category
+    code = models.CharField(max_length=10,)
+    title = models.TextField()
+
+    # keep track of date created
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    # keep track of updates
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('code',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return '{} - {}'.format(self.code, self.title[0:10])
+
+
+class Diagnosis(models.Model):
+    # category diagnosis falls under
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='diagnosis')
+
+    # numerical code for the diagnosis
+    code = models.IntegerField(null=True)
+
+    # full alphanumeric code for the diagnosis
+    full_code = models.CharField(max_length=10)
+
+    # short description for the diagnosis
+    abbreviated_description = models.TextField()
+
+    # fully describe the diagnosis
+    full_description = models.TextField()
+
+    # compatible with the previous version
+    backward_compatible = models.BooleanField(default=True)
+
+    # keep track of date created
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    # keep track of updates
+    updated = models.DateTimeField(auto_now=True)
